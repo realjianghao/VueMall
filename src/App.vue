dfd9d9fbd1fd92120c29afb714a2e0fe1c25acb9
Hao <jianghao0426>
2021-01-21 13:34:54 +0800

    更新首页视图

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tab-bar>
-      <tab-bar-item path="/home">
-        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/home_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category">
-        <img src="./assets/img/tabbar/category.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/category_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart">
-        <img src="./assets/img/tabbar/shopcart.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/shopcart_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <img src="./assets/img/tabbar/profile.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/profile_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from "@/components/tabbar/TabBar";
-import TabBarItem from "@/components/tabbar/TabBarItem";
+import MainTabBar from "@/components/content/mainTabBar/MainTabBar";
 export default {
   name: 'App',
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 }
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultidata} from "@/network/home";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data(){
+    return {
+      banners: [],
+      recommends: []
+
+    }
+  },
+  created(){
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
